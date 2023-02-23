f28494c3f3857969e103dbfeedae4e07e1cc6a61
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-02-23 00:38:12 +0000

    inserting history as html buttons to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,53 +4,8 @@ const resultsWrapper = $('main');
 const todayWeatherWrapper = $('#today');
 const forecastWrapper = $('#forecast');
 const forecastCardContainer = $('#forecast-list');
-
-
-// Update local storage with latest city Search
-function updateLocalStorage (cityName) {
-    // Store new entries as lower case
-    let newCityName = cityName.toLowerCase();
-    // create City object to cityName
-    let cityObj = {name: newCityName};
-
-    // Check for existing search history
-    if (localStorage.getItem('searchHistory') !== null) {
-        // Get existing search history
-        let searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
-        //If inputted city has been stored to search history in localStorage, 
-        for (let i = 0; i < searchHistory.length; i++) {
-            let city = searchHistory[i];
-            if( newCityName === city.name){
-                // remove the stored city name. 
-                // The name will be added in again as the "latest" when the list is updated 
-                searchHistory.splice(i, 1);
-            } 
-        }
-        // Update search history 
-        searchHistory.unshift(cityObj);
-        // Set the search history to localStorage
-        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-        
-        return searchHistory;
-    } else {
-        // Store new City in local storage
-        let newSearchHistory = [cityObj];
-        localStorage.setItem('searchHistory', JSON.stringify(newSearchHistory));
-        return newSearchHistory;
-    }
-
-}
-
-function updateSearchHistory(cityName) {
-    // Add city name to search history
-    //  - Get previous searches from localStorage
-    let historyArr = [updateLocalStorage(cityName)];
-
-    //   2. Show search history
-    //     - Pull search history from localStorage
-    //     - If search history is not empty, output each city to the search history display in the DOM
-}
-
+const searchHistoryContainer = $('#history');
+const historyPlaceholder = $('.no-search');
 
 function fetchWeatherInfo(city) {
 
@@ -97,6 +52,58 @@ function DisplayWeatherForecast(city) {
 
 }
 
+// Update local storage to add a city name to search history
+// - Get previous searches from localStorage
+// - Pull search history from localStorage
+// - If search history is not empty, output each city to the search history display in the DOM
+function updateLocalStorage (cityName) {
+    // Store new entries as lower case
+    let newCityName = cityName.toLowerCase();
+    // create City object to cityName
+    let cityObj = {name: newCityName};
+
+    // Check for existing search history
+    if (localStorage.getItem('searchHistory') !== null) {
+        // Get existing search history
+        let searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+        //If inputted city has been stored to search history in localStorage, 
+        for (let i = 0; i < searchHistory.length; i++) {
+            let city = searchHistory[i];
+            if( newCityName === city.name){
+                // remove the stored city name. 
+                // The name will be added in again as the "latest" when the list is updated 
+                searchHistory.splice(i, 1);
+            } 
+        }
+        // Update search history 
+        searchHistory.unshift(cityObj);
+        // Set the search history to localStorage
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+        
+        return searchHistory;
+    } else {
+        // Store new City in local storage
+        let newSearchHistory = [cityObj];
+        localStorage.setItem('searchHistory', JSON.stringify(newSearchHistory));
+        return newSearchHistory;
+    }
+}
+
+function updateSearchHistory(cityName) {
+    //Retrieve the search history from local storage
+    let historyArr = updateLocalStorage(cityName);
+
+    // Remove search history placeholder
+    historyPlaceholder.hide()
+
+    // console.log(historyArr);
+    // Insert HTML buttons for every answer choice
+    for (let search of historyArr) {
+        console.log(search);
+        $(`<button>${search.name}</button>`).appendTo(searchHistoryContainer);
+    }
+}
+
 function refreshSearchResults(inputText) {
     // Remove searched city from search field
     searchInput.val('');
@@ -128,7 +135,6 @@ function init() {
             updateSearchHistory(inputText);
             // Refresh the weather forecast
             refreshSearchResults(inputText);
-
         }
     });
 }
